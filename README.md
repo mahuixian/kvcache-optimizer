c5f70dc14b7c202eaae1f750099a08f64be885a2
mahuixian <mhx@DESKTOP-I3GDQNQ.localdomain>
2024-07-08 17:47:03 +0800

    add h2o

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-通过优化KV Cache，提升LLM提升长文本外推性能
+# 通过优化KV Cache，提升LLM提升长文本外推性能
 
-StreamingLLM：
+## StreamingLLM
 
 https://github.com/mit-han-lab/streaming-llm/tree/main
 
 streamingllm会单独分出一部分内存来存储和重用attention sink和rolling cache的KV状态，这样LLM的输入就可以无限长。streamingllm无法应用于使用了绝对位置编码的LLM。
 
 ![](/img/1.png)
+
+在多轮对话中应用streamingllm时发现头部attention sink的kvcache貌似没有被更新，实际上是因为在拼接后的头部token也会被分配不必要的强注意力，这一部分token没有实际的语义信息，但是由于具有强注意力，所以还是进行保留。
+
+## H2O:Heavy Hitter Oracle
+
+https://arxiv.org/pdf/2306.14048v1
+
+在Attention机制中，attention分数比较大的token对计算结果的影响比较大，H2O将这些tokens称为Heavy Hitters（H2），只要保证保留的KV Cache中存在H2，即可确保结果。所以H2O通过贪心算法，将当前token与其他token的注意力按列相加即得到当前token的重要性分数，然后将分数低的token丢弃即可。
+
+![image.png](/img/1720167597632-482e02cf-a37d-4429-92af-b2f6467cbca4.png)
+
+
+
+一般来说attention scores的size为（bsz, num_heads, seq_len, kv_len），需要先按照num_heads将attention scores相加，然后按照seq_len的维度将attention scores相加，得到最终的重要性分数。
--- kvcache/h2o.py
+++ kvcache/h2o.py
@@ -1,17 +1,63 @@
+import torch
+
+
+def slice1d(x, keep_indices):
+    return torch.cat([x[bsz, keep_indices[bsz], ...] for bsz in range(x.shape[0])], dim=0)
+
+
+def slice2d(x, keep_indices):
+    return torch.cat([x[bsz, :, keep_indices[bsz], ...] for bsz in range(x.shape[0])], dim=0)
+
+
+def slice3d(x, keep_indices):
+    return torch.cat([x[bsz, :, :, keep_indices[bsz], ...] for bsz in range(x.shape[0])], dim=0)
+
+
+DIM_TO_SLICE = {
+    1: slice1d,
+    2: slice2d,
+    3: slice3d,
+}
+
+
 class H2O:
     def __init__(
         self,
-        hh_size: int = 4,
-        rolling_size: int = 512,
-        k_seq_dim: int = 1,
-        v_seq_dim: int = 1
+        hh_size,
+        recent_size,
+        k_seq_dim=1,
+        v_seq_dim=1
     ):
         self.hh_size = hh_size
-        self.rolling_size = rolling_size
-        self.cache_size = hh_size + rolling_size
+        self.recent_size = recent_size
         self.k_seq_dim = k_seq_dim
         self.v_seq_dim = v_seq_dim
-        self.hh_score = None
+        self.cache_size = hh_size + recent_size
+        self.k_slice = DIM_TO_SLICE(k_seq_dim)
+        self.v_slice = DIM_TO_SLICE(v_seq_dim)
         
-    def __call__(self, past_key_values, attn_score_cache):
+    
+    def __call__(self, past_key_values, attn_scores):
+        if past_key_values is None:
+            return None
+        
+        kv_len = past_key_values[0][0].shape[self.k_seq_dim]
+        if kv_len <= self.cache_size:
+            return past_key_values
+        
+        #已测试，需要按照头将注意力合并，再去获取indices
+        attn_scores = attn_scores.sum(1).sum(1) #(bsz, num_heads, seq_len, kv_len) #(bsz, seq_len, kv_len) #(bsz, kv_len)
+        select_hh_scores = attn_scores[:, :-self.cache_size]
+        _, topk_indices = torch.topk(select_hh_scores, self.hh_size, dim=-1) #(bsz, hh_size)
+        keep_recent_indices = torch.arange(kv_len - self.cache_size, kv_len).unsqueeze(0).expand(topk_indices.shape[0], -1)
+        keep_indices = torch.cat([topk_indices, keep_recent_indices], dim=-1) #(bsz, self.cache_size)
+        assert keep_indices.size(-1) == self.cache_size
+        return [
+            (
+                self.k_slice(k, keep_indices),
+                self.v_slice(v, keep_indices)
+            )
+            for k, v in past_key_values
+        ]
+        
         
